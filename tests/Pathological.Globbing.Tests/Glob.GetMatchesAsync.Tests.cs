34992662ed4c2dacb3dfe25e9dc8e91bf2b25665
David Pine <david.pine.7@gmail.com>
2023-09-28 07:05:08 -0500

    Added another test

--- tests/Pathological.Globbing.Tests/Glob.GetMatchesAsync.Tests.cs
+++ tests/Pathological.Globbing.Tests/Glob.GetMatchesAsync.Tests.cs
@@ -7,6 +7,56 @@ public sealed partial class GlobTests(TempFolderTestFixture fixture)
     : IClassFixture<TempFolderTestFixture>
 {
     [Fact]
+    public async Task GetMatchesAsyncRespectsCancellationWithTimeSpanSignal()
+    {
+        string[] files =
+        [
+            "text-file.txt", "markdown.md", "image.png",
+        ];
+
+        string[] folders =
+        [
+            "folder1", "folder2", "folder3",
+        ];
+
+        foreach (var folder in folders)
+        {
+            var folderPath = Path.Combine(fixture.TempFolder, folder);
+            Directory.CreateDirectory(folderPath);
+
+            foreach (var file in files)
+            {
+                var filePath = Path.Combine(folderPath, file);
+                await File.WriteAllTextAsync(filePath, null);
+            }
+        }
+
+        var glob = new Glob(fixture.TempFolder);
+
+        var actualCount = 0;
+
+        try
+        {
+            await foreach (var file in glob.GetMatchesAsync(
+                patterns: ["**/*.md", "**/*.txt"],
+                ignorePatterns: ["folder3"],
+                signal: TimeSpan.FromMicroseconds(1)))
+            {
+                ++actualCount;
+                Assert.Contains(Path.GetFileName(file), files);
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            Assert.True(actualCount <= 4);
+        }
+        catch (OperationCanceledException)
+        {
+            Assert.True(true);
+        }        
+    }
+
+    [Fact]
     public async Task GetMatchesAsyncRespectsCancellationToken()
     {
         string[] files =
